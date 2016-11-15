42bfa97bdc069c6a9f5fb5783cb140d0cc81ab8b
kurochan <kuro@kurochan.org>
2016-11-15 21:19:12 +0900

    init record config

--- record.tf
+++ record.tf
@@ -1,6 +1,13 @@
-resource "cloudflare_record" "test_A" {
+resource "cloudflare_record" "chofu_tech_A" {
     domain = "chofu.tech"
-    name = "test"
-    value = "192.168.1.1"
+    name = "chofu.tech"
+    value = "151.101.100.133"
+    type = "A"
+}
+
+resource "cloudflare_record" "www_chofu_tech_A" {
+    domain = "chofu.tech"
+    name = "chofu.tech"
+    value = "151.101.100.133"
     type = "A"
 }
